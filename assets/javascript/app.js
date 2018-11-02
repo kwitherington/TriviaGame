e7f78d6a3fe9fe059be77e01b7866a2ec44f349b
KOV <43220938+kwitherington@users.noreply.github.com>
2018-11-02 18:04:07 -0500

    game structure script

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,110 @@
-var score = 20;
+var points = 20;
+var correct = 0;
+var incorrect = 0;
+var score = 0;
+var questionSelector = 0;
+var rightWrong = false;
 
-var question1Answers = [
-    "Good Kid M.A.A.D City",
+var questions = [
+    "What was Kendrick Lamar's first studio album?",
+    "Weezy F Baby and the F is NOT for...",
+    "Rap beef is part of the culture, one of these does NOT belong...?"
+];
+
+var answerArrays = [
+    [
+        "Good Kid M.A.A.D City",
+        "Section.80",
+        "Damn.",
+        "To Pimp a Butterfly"
+    ],
+    [
+        "Front Door",
+        "Forensics",
+        "Finisher",
+        "Flaw"
+    ],
+    [
+        "Kendrick vs J. Cole",
+        "Tupac vs Biggie",
+        "Jay-Z vs Nas",
+        "50 Cent vs Ja Rule"
+    ]
+];
+
+
+var correctAnswers = [
     "Section.80",
-    "Damn.",
-    "To Pimp a Butterfly"
-]
+    "Flaw",
+    "Kendrick vs J. Cole"
+];
 
 
-$("#startButton").click(displayGame);
 
-function displayGame() {
-    $("#start").html("");
-    $("#score").text(score);
-    $("#question").text("What was Kendrick Lamar's first studio album?");
-    for (i = 0; i < question1Answers.length; i++) {
-        var answerButton = $("<button>");
-        answerButton.addClass("answer");
-        answerButton.id = "answer" + i;
-        answerButton.text(question1Answers[i]);
-        $("#answerSection" + i).append(answerButton);
+
+$(document).ready(function() {
+    $("#startButton").click(displayGame);
+
+    function displayGame() {
+
+        $("#rightWrong").text("");
+        $("#prompt").text("");
+        $("#start").html("");
+        $("#points").text(points);
+        $("#question").text(questions[questionSelector]);
+        for (i = 0; i < answerArrays[questionSelector].length; i++) {
+            $("#answerSection" + i).html("");
+            var answerButton = $("<button>");
+            answerButton.addClass("answer");
+            answerButton.id = "answer" + i;
+            answerButton.attr("data-name", answerArrays[questionSelector][i]);
+            answerButton.text(answerArrays[questionSelector][i]);
+            $("#answerSection" + i).append(answerButton);
+        }
+        
     }
-  }
+
+    $("div.row.answers").on("click", "button.answer", userSelection)
+
+    function userSelection() {
+        if ($(this).attr("data-name") == correctAnswers[questionSelector]) {
+            rightWrong = true;
+            correct++;
+            var newScore = score += points;
+            $("#score").text(newScore);
+        } else {
+            incorrect++ 
+        }
+        showResult();
+        nextQuestion();
+        setTimeout(displayGame, 3000);
+    }
+
+    function nextQuestion() {
+        if (questionSelector == answerArrays.length - 1) {
+            questionSelector = 0;
+        } else {
+            questionSelector++;
+        }
+    }
+
+    function showResult() {
+        clearGame();
+        if (rightWrong == true) {
+            $("#rightWrong").text("Correct");
+            rightWrong = false;
+        } else {
+            $("#rightWrong").text("Incorrect");
+            $("#prompt").text("The correct answer was: " + correctAnswers[questionSelector]);
+        }
+    }
+
+    function clearGame() {
+        $("#question").text("");
+        for (i = 0; i < answerArrays[questionSelector].length; i++) {
+            $("#answerSection" + i).html("");
+        }
+    }
+
+})
+
