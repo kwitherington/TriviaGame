4b70faff9f06e2496eeed5b2922352de6c506bb5
KOV <43220938+kwitherington@users.noreply.github.com>
2018-11-02 19:26:42 -0500

    timer and reset added with notes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,24 +32,24 @@ var answerArrays = [
     ]
 ];
 
-
 var correctAnswers = [
     "Section.80",
     "Flaw",
     "Kendrick vs J. Cole"
 ];
 
-
-
-
 $(document).ready(function() {
     $("#startButton").click(displayGame);
 
+    // Clears start screen, previous game scorecard, or prompt 
+    // and displays a question with answer buttons
     function displayGame() {
-
-        $("#rightWrong").text("");
-        $("#prompt").text("");
+        clearPrompt();
         $("#start").html("");
+        $("#playAgainButtonSection").html("");
+        $("#rightAnswers").text("");
+        $("#wrongAnswers").text("");
+        $("#totalScore").text("");
         $("#points").text(points);
         $("#question").text(questions[questionSelector]);
         for (i = 0; i < answerArrays[questionSelector].length; i++) {
@@ -60,12 +60,13 @@ $(document).ready(function() {
             answerButton.attr("data-name", answerArrays[questionSelector][i]);
             answerButton.text(answerArrays[questionSelector][i]);
             $("#answerSection" + i).append(answerButton);
-        }
-        
+        }  
     }
 
+    // Grabs dynamically created answer buttons and runs right/wrong logic.
     $("div.row.answers").on("click", "button.answer", userSelection)
 
+    // determines if the user selection was correct and adjusts points accordingly.
     function userSelection() {
         if ($(this).attr("data-name") == correctAnswers[questionSelector]) {
             rightWrong = true;
@@ -77,17 +78,20 @@ $(document).ready(function() {
         }
         showResult();
         nextQuestion();
-        setTimeout(displayGame, 3000);
     }
 
+    // Determines whether to show the next question or show scorecard.
     function nextQuestion() {
         if (questionSelector == answerArrays.length - 1) {
             questionSelector = 0;
+            setTimeout(showScoreCard, 2500);
         } else {
             questionSelector++;
+            setTimeout(displayGame, 2500);
         }
     }
 
+    // Displays result of the question and prompt if wrong.
     function showResult() {
         clearGame();
         if (rightWrong == true) {
@@ -99,11 +103,39 @@ $(document).ready(function() {
         }
     }
 
+    // Shows the results from the game, creates play button, 
+    // clears the prompt, and resets ints for next game.
+    function showScoreCard() {
+        clearPrompt();
+        $("#points").text("");
+        $("#score").text("");
+        $("#rightAnswers").text("Correct Answers: " + correct);
+        $("#wrongAnswers").text("Incorrect Answers: " + incorrect);
+        $("#totalScore").text("Total Score: " + score);
+        var playAgainButton = $("<button>");
+        playAgainButton.addClass("playAgain");
+        playAgainButton.text("Play Again?");
+        $("#playAgainButtonSection").append(playAgainButton);
+        correct = 0;
+        incorrect = 0;
+        score = 0;
+    }
+
+    // click functionality for "play again" button.
+    $(document).on("click", "button.playAgain", displayGame);
+
+    // Clears the question and answer buttons from their divs.
     function clearGame() {
         $("#question").text("");
         for (i = 0; i < answerArrays[questionSelector].length; i++) {
             $("#answerSection" + i).html("");
         }
+    }
+
+    // Clears prompt.
+    function clearPrompt() {
+        $("#rightWrong").text("");
+        $("#prompt").text("");
     }
 
 })
